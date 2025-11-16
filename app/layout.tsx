706ee4437d0a124b2a5917a09c6949df7845b249
NLA <dev@nlaconsultoria.com.br>
2025-11-16 18:31:47 -0300

    Config pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,30 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             posthog.init('${env.POSTHOG_KEY}',{api_host:'https://app.posthog.com'});
           `}</Script>
         )}
+        {env.META_PIXEL_ID && (
+          <>
+            <Script id="meta-pixel" strategy="afterInteractive">{`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${env.META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}</Script>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${env.META_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
         <Script id="org-jsonld" type="application/ld+json" strategy="afterInteractive">
           {JSON.stringify(jsonOrg)}
         </Script>
--- lib/env.ts
+++ lib/env.ts
@@ -5,7 +5,8 @@ export const env = {
   GTM_ID: process.env.NEXT_PUBLIC_GTM_ID || "",
   POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
   SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || "",
+  META_PIXEL_ID: process.env.NEXT_PUBLIC_META_PIXEL_ID || "",
+  META_PIXEL_ACCESS_TOKEN: process.env.META_PIXEL_ACCESS_TOKEN || "",
 };
 
-export const hasAnalytics = () => Boolean(env.GTM_ID || env.POSTHOG_KEY);
-
+export const hasAnalytics = () => Boolean(env.GTM_ID || env.POSTHOG_KEY || env.META_PIXEL_ID);
