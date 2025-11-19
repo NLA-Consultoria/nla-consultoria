a62c34c73b4ec8edb382815661346fb6fa805322
NLA <dev@nlaconsultoria.com.br>
2025-11-19 19:08:17 -0300

    Config pixel meta

--- lib/env.ts
+++ lib/env.ts
@@ -2,11 +2,18 @@ export const env = {
   N8N_WEBHOOK_URL: process.env.NEXT_PUBLIC_N8N_WEBHOOK_URL || "",
   AGENDA_URL: process.env.NEXT_PUBLIC_AGENDA_URL || "",
   WHATSAPP_URL: process.env.NEXT_PUBLIC_WHATSAPP_URL || "",
-  GTM_ID: process.env.NEXT_PUBLIC_GTM_ID || "",
+  GTM_ID: process.env.NEXT_PUBLIC_GTM_ID || process.env.GTM_ID || "",
   POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
   SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || "",
-  META_PIXEL_ID: process.env.NEXT_PUBLIC_META_PIXEL_ID || "",
-  META_PIXEL_ACCESS_TOKEN: process.env.META_PIXEL_ACCESS_TOKEN || "",
+  META_PIXEL_ID:
+    process.env.NEXT_PUBLIC_META_PIXEL_ID ||
+    process.env.META_PIXEL_ID ||
+    "",
+  META_PIXEL_ACCESS_TOKEN:
+    process.env.META_PIXEL_ACCESS_TOKEN ||
+    process.env.NEXT_PUBLIC_META_PIXEL_ACCESS_TOKEN ||
+    "",
 };
 
-export const hasAnalytics = () => Boolean(env.GTM_ID || env.POSTHOG_KEY || env.META_PIXEL_ID);
+export const hasAnalytics = () =>
+  Boolean(env.GTM_ID || env.POSTHOG_KEY || env.META_PIXEL_ID);
