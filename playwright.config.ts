5494faa1fddb1c4fc670a87b3e05df3e347e2ba7
Murillo Alves <murillo.prod@gmail.com>
2026-01-08 16:37:02 -0300

    Corrige config Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
     trace: "on-first-retry",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    reducedMotion: "no-preference",
+    contextOptions: {
+      reducedMotion: "no-preference",
+    },
   },
   projects: [
     {
